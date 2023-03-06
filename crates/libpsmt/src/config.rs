3001e3179fd2f86f1e93d924612a465de644ddf3
Pablo Hernández <17086478+Hadronomy@users.noreply.github.com>
2023-03-06 12:19:18 +0000

    docs: add inline documentation

--- crates/libpsmt/src/config.rs
+++ crates/libpsmt/src/config.rs
@@ -4,11 +4,32 @@ use serde::{Deserialize, Serialize};
 
 use crate::Error;
 
+/// Configuration
+/// of a psmt project
+///
+/// # Examples
+///
+/// ```
+/// # use eyre::Result;
+/// use libpsmt::ProjectConfig;
+///
+/// # fn my_fn() -> Result<()> {
+/// // Read project configuration
+/// let my_config = ProjectConfig::read()?;
+/// // Get string equivalent
+/// let config_str = my_config.to_string()?;
+/// println!("{}", config_str);
+/// # Ok(())
+///
+/// # }
+/// ```
 #[derive(Debug, Deserialize, Serialize)]
 pub struct ProjectConfig {
     template: Option<TemplateConfig>,
 }
 
+/// Configuration of a psmt **template**
+/// project
 #[derive(Debug, Deserialize, Serialize)]
 pub struct TemplateConfig {
     name: String,
@@ -18,6 +39,11 @@ pub struct TemplateConfig {
     variables: Vec<TemplateVariable>,
 }
 
+/// A variable that exists inside the
+/// template project.
+///
+/// This variables will be replaced
+/// by the user inputted value
 #[derive(Debug, Deserialize, Serialize)]
 pub struct TemplateVariable {
     name: String,
@@ -26,6 +52,7 @@ pub struct TemplateVariable {
 }
 
 // TODO: Make this API Make sense
+
 impl Default for ProjectConfig {
     fn default() -> Self {
         Self {
@@ -50,8 +77,10 @@ impl Default for ProjectConfig {
     }
 }
 
+// TODO: Add comment support when writting
+
 impl ProjectConfig {
-    pub fn build() -> Result<Config, Error> {
+    fn build() -> Result<Config, Error> {
         let config = Config::builder()
             .add_source(File::with_name("psmt.toml"))
             .build()?;
